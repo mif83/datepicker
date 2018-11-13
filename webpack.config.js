96d31e20e5169758636be21d904da11f633d3185
Yesypenko Dmitry <dima983@gmail.com>
2018-11-13 10:45:40 +0200

    create /docs folder for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     devtool: 'inline-source-map',
     devServer: {
-         contentBase: './dist'
+         contentBase: './docs'
     },
 	plugins: [
 		new CopyWebpackPlugin([{ from: './src/*', flatten: true}],   { ignore: [ '*.js'] })
